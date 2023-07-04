e152017c3c3f13a5edaa4578e7380b3713317309
Emily Stevenson <e.stevenson6@hotmail.co.uk>
2023-07-04 11:33:14 +0100

    editing api url

--- src/pages/CreateMeme.js
+++ src/pages/CreateMeme.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { Button } from "@mantine/core";
 
 function CreateMeme() {
-  const API_URL = "http://localhost:5005";
+  const API_URL = process.env.REACT_API_URL || "http://localhost:5005";
   const [template, setTemplate] = useState(null);
   const [input, setInput] = useState({});
   const [title, setTitle] = useState("");
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,7 +4,7 @@ import TemplateCard from "../components/TemplateCard";
 import { Badge, Button, Card, Grid, Group, Image, Text } from "@mantine/core";
 
 function HomePage() {
-  const API_URL = "http://localhost:5005";
+  const API_URL = process.env.REACT_API_URL || "http://localhost:5005";
   const [templates, setTemplates] = useState([]);
 
   const getAllTemplates = () => {
@@ -27,7 +27,6 @@ function HomePage() {
           <TemplateCard key={template.id} {...template} />
         ))}
       </Grid>
-      ;
     </div>
   );
 }
